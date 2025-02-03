f1910c501ac56eae661dc049f62eb995291e2f39
moccad3v <jdavidpascuas@gmail.com>
2025-02-03 18:14:35 -0500

    Fixed distribution on about view

---
+++ about.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+  <!-- Navbar Start -->
+  <?php include 'navbar.html'; ?>
+  <!-- Navbar End -->
+
+  <!-- Header Start -->
+  <div class="container-fluid bg-secondary mb-5">
+    <div
+      class="d-flex flex-column align-items-center justify-content-center"
+      style="min-height: 100px ">
+      <h3 class="display-3 font-weight-bold text-white mt-3">Nosotros</h3>
+    </div>
+  </div>
+  <!-- Header End -->
+
+  <!-- About Start -->
+  <div class="container-fluid py-4">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-5">
+          <img
+            class="img-fluid rounded mb-5 mb-lg-0"
+            src="img/about-1.jpg"
+            alt="" />
+        </div>
+        <div class="col-lg-7">
+          <p class="section-title pr-5">
+            <span class="pr-2">Conozca más de nosotros</span>
+          </p>
+          <h1 class="mb-4">Educación de Calidad para tus hijos</h1>
+          <p>
+            Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo
+            dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea justo.
+            Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est
+            dolor
+          </p>
+          <div class="row pt-2 pb-4">
+            <div class="col-6 col-md-4">
+              <img class="img-fluid rounded" src="img/about-2.jpg" alt="" />
+            </div>
+            <div class="col-6 col-md-8">
+              <ul class="list-inline m-0">
+                <li class="py-2 border-top border-bottom">
+                  <i class="fa fa-check text-primary mr-3"></i>Educación en Valores
+                </li>
+                <li class="py-2 border-bottom">
+                  <i class="fa fa-check text-primary mr-3"></i>Educación en Proyectos
+                </li>
+                <li class="py-2 border-bottom">
+                  <i class="fa fa-check text-primary mr-3"></i>Educación en Emprendimiento
+                </li>
+              </ul>
+            </div>
+          </div>
+          <a href="" class="btn btn-secondary mt-2 py-2 px-4">Leer más</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- About End -->
+
+  <!-- Facilities Start -->
+  <div class="container-fluid pt-5">
+    <div class="container pb-3">
+      <div class="row">
+        <div class="col-lg-5 col-md-6 pb-1">
+          <div
+            class="d-flex bg-light shadow-sm border-top rounded mb-4"
+            style="padding: 30px">
+            <i
+              class=""></i>
+            <div class="pl-4">
+              <h4>Misión</h4>
+              <p class=" text-align: justify"></p>
+              <p class="m-0">
+                Glorificar a Dios y bajo la influencia del Espíritu
+                santo, guiar a cada miembro de la
+                Corporación Educativa Adventista C.E.A. a una
+                experiencia personal y transformadora con Cristo, que lo capacite
+                como discípulo para el servicio a Dios y a la sociedad.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-7 col-md-6 pb-1">
+          <div
+            class="d-flex bg-light shadow-sm border-top rounded mb-4"
+            style="padding: 30px">
+            <i
+              class=""></i>
+            <div class="pl-4">
+              <h4>Visión</h4>
+              <p class=" text-align: justify"></p>
+              <p class="m-0">
+                Para el año 2025 ser reconocidos como el más
+                destacado colegio cristiano de la ciudad de
+                Cali, teniendo como modelo y guía a Cristo
+                Jesús, en la formación de estudiantes
+                con un proyecto de vida integral, que responda a los
+                retos de una sociedad en constante cambio,
+                donde el liderazgo, el servicio y los valores
+                bíblicos se constituyen en los pilares
+                fundamentales del proceso de formación.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-12 col-md-6 pb-1">
+          <div
+            class="d-flex bg-light shadow-sm border-top rounded mb-4"
+            style="padding: 30px">
+            <i
+              class="flaticon-030-crayons h1 font-weight-normal text-primary mb-3"></i>
+            <div class="pl-4">
+              <h4>Propósitos Institucionales</h4>
+              <p class=" text-align: justify"></p>
+              <p class="m-0">
+                La razón de ser de la Corporación es dar a conocer a Jesús como Salvador, fundamentar a los
+                estudiantes en su vida espiritual por medio del estudio de la Biblia, e impactar a la comunidad
+                por medio de los diferentes proyectos.
+                <br><br>
+                La Corporación abre también sus puertas, sin distinción de sexo, raza, credo o color a todos los
+                estudiantes y padres de familia y/o acudientes de nobles ideales, que deseen acudir a sus aulas
+                para aprovechar las ventajas del modelo pedagógico cristiano Adventista del Séptimo Día.
+                Este modelo educativo es altamente reconocido por tener un comprobado sentido de pertenencia
+                Cristo céntrica en la formación de personas de gran conducta social y espiritual. Por eso
+                considera como fundamentos la dirección del Espíritu Santo, como agente educativo divino,
+                la revelación de Dios en la naturaliza, la Biblia y en el ejemplo de Jesucristo como
+                Maestro Modelo, y los principios educativos bosquejados en el libro La Educación de
+                Elena G. de White, donde se destacan el propósito redentor de la verdadera educación, como es
+                la restauración de los seres humanos a la imagen de su Hacedor.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-12 col-md-6 pb-1">
+
+          <div
+
+            class="d-flex bg-light shadow-sm border-top rounded mb-4"
+            style="padding: 30px">
+            <i
+              class="flaticon-017-toy-car h1 font-weight-normal text-primary mb-3"></i>
+
+            <div class="pl-4">
+              <h4>Valores Institucionales</h4>
+              <p class="m-0">
+                Los valores institucionales son los siguientes: fidelidad, integridad, perseverancia, libertad,
+                sabiduría, respeto, tolerancia, servicio, compromiso, excelencia, obediencia y amor, esfuerzo,
+                devoción, esperanza, saber escuchar, tomar decisiones, y unidad.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-12 col-md-6 pb-1">
+          <div
+            class="d-flex bg-light shadow-sm border-top rounded mb-4"
+            style="padding: 30px">
+            <i
+              class="flaticon-025-sandwich h1 font-weight-normal text-primary mb-3"></i>
+            <div class="pl-4">
+              <h4>Directrices generales de la pedagogía Adventista del Séptimo Día</h4>
+              <p class="m-0">
+                Desarrollo de procesos pedagógico que propician y potencian las
+                capacidades éticas, cognitivas, comunicativas, afectivas, psicomotrices, de
+                relación interpersonal y de compromiso social. Estos procesos se logran mediante:
+                <br><br>
+                Respeto e identidad con las verdades bosquejadas en la Santa Biblia como revelación
+                especial de Dios, tal como las enseña la Iglesia Adventista del Séptimo Día.
+                No obstante, la Corporación admite como alumnos y padres de familia de otros credos
+                que aceptan estas verdades fundamentales para la formación de sus hijos, sin que estos
+                sean obligados a afiliarse a la denominación Adventista del Séptimo Día.
+                Pero a la vez, la Corporación desautoriza la enseñanza y proselitismo de otros
+                credos religiosos, tanto por parte de cualquier miembro de su comunidad educativa.
+                <br><br>
+                Pedagogía construida en tres pilares: La formación física integral (simbolizada por la mano),
+                la formación cognitiva integral (simbolizada por la mente) y la formación espiritual integral
+                (simbolizada por el corazón). Pero de los tres pilares, la Corporación le da más énfasis a la
+                formación espiritual.
+                <br><br>
+                Jesucristo es el Modelo perfecto de la comunidad educativa de la Corporación.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Facilities Start -->
+
+  <!-- Team Start -->
+  <div class="container">
+    <div class="text-center pb-2">
+      <p class="section-title px-5">
+        <!-- <span class="px-2">Popular Classes</span>-->
+      </p>
+      <h1 class="mb-4">Conoce nuestra oferta educativa </h1>
+      <p>
+        <a href="contact.html" class="btn btn-secondary px-4">Contáctanos</a>
+      </p>
+    </div>
+  </div>
+
+  <!-- Team End -->
+
+  <!-- Footer Start -->
+  <div
+    class="container-fluid bg-secondary text-white mt-5 py-5 px-sm-3 px-md-5">
+    <div class="row pt-5">
+      <div class="col-lg-3 col-md-6 mb-5">
+        <a
+          href=""
+          class="navbar-brand font-weight-bold text-primary m-0 mb-4 p-0"
+          style="font-size: 40px; line-height: 40px">
+          <!--<i class="flaticon-043-teddy-bear"></i>-->
+          <span class="text-white">CEA Cámbulos</span>
+        </a>
+        <p>
+          Colegio Cristiano, Educación Preescolar, Primaria, Secundaria, Bachillerato,
+          Valores, Cristiana, Cali, Colombia.
+          Reconocimiento Oficial de Estudios No. 672 del 16/06/1999
+          Nueva Resolución 2763 del 19/08/2004
+        </p>
+        <div class="d-flex justify-content-start mt-4">
+          <a
+            class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
+            style="width: 38px; height: 38px"
+            href="#"><i class="fab fa-twitter"></i></a>
+          <a
+            class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
+            style="width: 38px; height: 38px"
+            href="#"><i class="fab fa-facebook-f"></i></a>
+          <a
+            class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
+            style="width: 38px; height: 38px"
+            href="#"><i class="fab fa-linkedin-in"></i></a>
+          <a
+            class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
+            style="width: 38px; height: 38px"
+            href="#"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-5">
+        <h3 class="text-primary mb-4">Contacto</h3>
+        <div class="d-flex">
+          <h4 class="fa fa-map-marker-alt text-primary"></h4>
+          <div class="pl-3">
+            <h5 class="text-white">Dirección</h5>
+            <p>Carrera 44 No. 9-25 Barrio: Cámbulos – Cali – Colombia</p>
+          </div>
+        </div>
+        <div class="d-flex">
+          <h4 class="fa fa-envelope text-primary"></h4>
+          <div class="pl-3">
+            <h5 class="text-white">Email</h5>
+            <p>info@cea.edu.co</p>
+          </div>
+        </div>
+        <div class="d-flex">
+          <h4 class="fa fa-phone-alt text-primary"></h4>
+          <div class="pl-3">
+            <h5 class="text-white">Teléfono</h5>
+            <p>314 8714906</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-5">
+        <h3 class="text-primary mb-4">Acceso Rápido</h3>
+        <div class="d-flex flex-column justify-content-start">
+          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Inicio</a>
+          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Conócenos</a>
+          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Admisiones</a>
+          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Proyectos</a>
+          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Finanzas</a>
+          <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Contacto</a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-5">
+        <h3 class="text-primary mb-4">Boletín</h3>
+        <form action="">
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control border-0 py-4"
+              placeholder="Nombre completo"
+              required="required" />
+          </div>
+          <div class="form-group">
+            <input
+              type="email"
+              class="form-control border-0 py-4"
+              placeholder="Email"
+              required="required" />
+          </div>
+          <div>
+            <button
+              class="btn btn-primary btn-block border-0 py-3"
+              type="submit">
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div
+      class="container-fluid pt-5"
+      style="border-top: 1px solid rgba(23, 162, 184, 0.2) ;">
+      <!--<p class="m-0 text-center text-white">
+          &copy;
+          <a class="text-primary font-weight-bold" href="#">Your Site Name</a>.
+          All Rights Reserved.-->
+
+      <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+      <!--Designed by
+          <a class="text-primary font-weight-bold" href="https://htmlcodex.com"
+            >HTML Codex</a
+          >-->
+      <!--<br />Distributed By:
+          <a href="https://themewagon.com" target="_blank">ThemeWagon</a>-->
+      </p>
+    </div>
+  </div>
+  <!-- Footer End -->
+
+  <!-- Back to Top -->
+  <a href="#" class="btn btn-primary p-3 back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+  <!-- Contact Javascript File -->
+  <script src="mail/jqBootstrapValidation.min.js"></script>
+  <script src="mail/contact.js"></script>
+
+  <!-- Template Javascript -->
+  <script src="js/main.js"></script>
+</body>
+
+</html>
