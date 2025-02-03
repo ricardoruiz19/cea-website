553e18e5208d72d8edb4dbf77fb13bd5e7138bfd
moccad3v <jdavidpascuas@gmail.com>
2025-02-03 14:23:50 -0500

    Created general navbar and footer, added to contact view

---
+++ contact.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<body>
+  <!-- Incluir Navbar -->
+  <?php include 'navbar.html'; ?>
+
+  <!-- Header -->
+  <div class="container-fluid bg-secondary mb-5">
+    <div
+      class="d-flex flex-column align-items-center justify-content-center"
+      style="min-height: 100px">
+      <h3 class="display-3 font-weight-bold text-white">Contacto</h3>
+      <div class="d-inline-flex text-white">
+        <!-- <p class="m-0"><a class="text-white" href="./index.html">Inicio</a></p>
+          <p class="m-0 px-2">/</p>
+          <p class="m-0"><a class="text-white" href="./class.html">Admisiones</p>-->
+      </div>
+    </div>
+  </div>
+  <!-- Header End -->
+
+  <!-- Contact Start -->
+  <div class="container-fluid pt-5">
+    <div class="container">
+      <div class="text-align: left pb-2">
+        <h1 class="mb-4">Déjanos tus datos y nos comunicaremos
+          <br> contigo lo más pronto posible
+        </h1>
+      </div>
+      <iframe loading="lazy" src="https://docs.google.com/forms/d/e/1FAIpQLSc-eMrtU7g-uMUIzSxaPkWMoGdqae4ItUDHxXh2tWvKQnVHSQ/viewform?embedded=true"
+        width="600" height="829" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+
+      <div class="elementor-element elementor-element-f922662 elementor-widget elementor-widget-google_maps" data-id="f922662" data-element_type="widget" data-widget_type="google_maps.default">
+        <div class="elementor-widget-container">
+          <div class="elementor-custom-embed">
+            <iframe frameborder="0" scrolling="no" marginheight="5" marginwidth="5"
+              src="https://maps.google.com/maps?q=Carrera%2044%20No.%209-25%20cali&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near"
+              aria-label="Carrera 44 No. 9-25 cali"></iframe>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- <div class="row">
+          <div class="col-lg-7 mb-5">
+            <div class="contact-form">
+              <div id="success"></div>
+              <form name="sentMessage" id="contactForm" novalidate="novalidate">
+                <div class="control-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    placeholder="Your Name"
+                    required="required"
+                    data-validation-required-message="Please enter your name"
+                  />
+                  <p class="help-block text-danger"></p>
+                </div>
+                <div class="control-group">
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    placeholder="Your Email"
+                    required="required"
+                    data-validation-required-message="Please enter your email"
+                  />
+                  <p class="help-block text-danger"></p>
+                </div>
+                <div class="control-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="subject"
+                    placeholder="Subject"
+                    required="required"
+                    data-validation-required-message="Please enter a subject"
+                  />
+                  <p class="help-block text-danger"></p>
+                </div>
+                <div class="control-group">
+                  <textarea
+                    class="form-control"
+                    rows="6"
+                    id="message"
+                    placeholder="Message"
+                    required="required"
+                    data-validation-required-message="Please enter your message"
+                  ></textarea>
+                  <p class="help-block text-danger"></p>
+                </div>
+                <div>
+                  <button
+                    class="btn btn-primary py-2 px-4"
+                    type="submit"
+                    id="sendMessageButton"
+                  >
+                    Send Message
+                  </button>-->
+    </div>
+    </form>
+  </div>
+  </div>
+  <div class="col-lg-5 mb-5">
+    <p>
+
+
+      CORPORACIÓN EDUCATIVA ADVENTISTA C.E.A CÁMBULOS
+      Carrera 44 No. 9-25 Barrio: Cámbulos – Cali – Colombia.
+      Teléfono: 572 5529457 | Móvil: 314 8714906 | A.A. 1269
+      info@cea.edu.co | www.cea.edu.co
+    </p>
+    <div class="d-flex">
+      <i
+        class="fa fa-map-marker-alt d-inline-flex align-items-center justify-content-center bg-primary text-secondary rounded-circle"
+        style="width: 45px; height: 45px"></i>
+      <div class="pl-3">
+        <h5>Address</h5>
+        <p>123 Street, New York, USA</p>
+      </div>
+    </div>
+    <div class="d-flex">
+      <i
+        class="fa fa-envelope d-inline-flex align-items-center justify-content-center bg-primary text-secondary rounded-circle"
+        style="width: 45px; height: 45px"></i>
+      <div class="pl-3">
+        <h5>Email</h5>
+        <p>info@example.com</p>
+      </div>
+    </div>
+    <div class="d-flex">
+      <i
+        class="fa fa-phone-alt d-inline-flex align-items-center justify-content-center bg-primary text-secondary rounded-circle"
+        style="width: 45px; height: 45px"></i>
+      <div class="pl-3">
+        <h5>Teléfono</h5>
+        <p>+012 345 67890</p>
+      </div>
+    </div>
+    <div class="d-flex">
+      <i
+        class="far fa-clock d-inline-flex align-items-center justify-content-center bg-primary text-secondary rounded-circle"
+        style="width: 45px; height: 45px"></i>
+      <div class="pl-3">
+        <h5>Opening Hours</h5>
+        <strong>Sunday - Friday:</strong>
+        <p class="m-0">08:00 AM - 05:00 PM</p>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+
+  <!-- Incluir Footer -->
+  <?php include 'footer.html'; ?>
+</body>
+
+</html>
