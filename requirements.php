e6a2d6f2163e170276b3c2dc9894b74bd7ddcc03
moccad3v <jdavidpascuas@gmail.com>
2025-02-04 17:17:09 -0500

    Added navbar and footer to requirements and seedbed view

--- requirements.php
+++ requirements.php
@@ -1,117 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="utf-8" />
-  <title>Educación Adventista</title>
-  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-  <meta content="Free HTML Templates" name="keywords" />
-  <meta content="Free HTML Templates" name="description" />
-
-  <!-- Favicon -->
-  <link href="img/favicon.ico" rel="icon" />
-
-  <!-- Google Web Fonts -->
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Handlee&family=Nunito&display=swap"
-    rel="stylesheet" />
-
-  <!-- Font Awesome -->
-  <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
-    rel="stylesheet" />
-
-  <!-- Flaticon Font -->
-  <link href="lib/flaticon/font/flaticon.css" rel="stylesheet" />
-
-  <!-- Libraries Stylesheet -->
-  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
-  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" />
-
-  <!-- Customized Bootstrap Stylesheet -->
-  <link href="css/style.css" rel="stylesheet" />
-</head>
-
 <body>
   <!-- Navbar Start -->
-  <div class="container-fluid bg-light position-relative shadow">
-    <nav
-      class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0 px-lg-5">
-      <a
-        href=""
-        class="navbar-brand font-weight-bold text-secondary"
-        style="font-size: 50px">
-        <logo>
-          <img src="img/logo2.png" alt="" style="width: 128px" />
-      </a>
-      <button
-        type="button"
-        class="navbar-toggler"
-        data-toggle="collapse"
-        data-target="#navbarCollapse">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div
-        class="collapse navbar-collapse justify-content-between"
-        id="navbarCollapse">
-        <div class="navbar-nav font-weight-bold mx-auto py-0">
-          <a href="index.php" class="nav-item nav-link">INICIO</a>
-          <a href="about.php" class="nav-item nav-link">CONÓCENOS</a>
-          <div class="nav-item dropdown">
-            <a
-              href="requirements.html"
-              class="nav-link dropdown-toggle"
-              data-toggle="dropdown">ADMISIONES</a>
-            <div class="dropdown-menu rounded-0 m-0">
-              <a href="requirements.html" class="dropdown-item">Requisitos</a>
-              <a href="https://drive.google.com/file/d/12PymyOwIdl137Rn0G6qGK2qO4fYcg8Un/view" class="dropdown-item">Datos Sensibles</a>
-            </div>
-          </div>
-          <div class="nav-item dropdown">
-            <a
-              href="https://www.cea.edu.co/proyectos/"
-              class="nav-link dropdown-toggle"
-              data-toggle="dropdown">PROYECTOS</a>
-            <div class="dropdown-menu rounded-0 m-0">
-              <a href="seedbed.php" class="dropdown-item">Semillero</a>
-              <a href="single.html" class="dropdown-item">Lúdico</a>
-              <a href="" class="dropdown-item">CEA Emprendedor</a>
-              <a href="" class="dropdown-item">CEA Valores</a>
-              <a href="" class="dropdown-item">CEA Verde</a>
-            </div>
-          </div>
-          <a href="finances.php" class="nav-item nav-link">FINANZAS</a>
-          <!--  <div class="nav-item dropdown">
-              <a
-                href="#"
-                class="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                >Pages</a
-              >
-              <div class="dropdown-menu rounded-0 m-0">
-                <a href="blog.html" class="dropdown-item">Blog Grid</a>
-                <a href="single.html" class="dropdown-item">Blog Detail</a>
-              </div>
-            </div> -->
-          <a href="news.php" class="nav-item nav-link">NOTICIAS</a>
-          <a href="contact.php" class="nav-item nav-link">CONTACTO</a>
-        </div>
-        <!-- <a href="" class="btn btn-primary px-4">Join Class</a> -->
-
-      </div>
-    </nav>
-
-  </div>
-
+  <?php include 'navbar.html'; ?>
   <!-- Navbar End -->
 
   <!-- Header Start -->
-  <div class="container-fluid bg-secondary mb-5">
+  <div class="container-fluid bg-secondary mb-2">
     <div
       class="d-flex flex-column align-items-center justify-content-center"
       style="min-height: 100px">
-      <h3 class="display-3 font-weight-bold text-white">Admisiones</h3>
+      <h3 class="display-3 font-weight-bold text-white mt-3">Admisiones</h3>
       <div class="d-inline-flex text-white">
         <!-- <p class="m-0"><a class="text-white" href="./index.php">Inicio</a></p>
           <p class="m-0 px-2">/</p>
@@ -292,222 +192,17 @@
         <div class="contenedor-imagen">
           <img src="img/requisitos2.jpg" alt="Imagen 2">
         </div>
-
-        <!-- <p class="section-title pr-5"> -->
-        <!--  <span class="pr-2">Book A Seat</span>
-            </p>-->
         <div class="card-body text-center">
-          <br> <br>
           <h1 class="mb-4">Conoce nuestra oferta educativa</h1>
-          <p>
-
-          </p>
           <a href="contact.php" class="btn btn-secondary px-4">Contáctanos</a>
-
-          <!-- <ul class="list-inline m-0">
-              <li class="py-2">
-                <i class="fa fa-check text-success mr-3"></i>Labore eos amet
-                dolor amet diam
-              </li>
-              <li class="py-2">
-                <i class="fa fa-check text-success mr-3"></i>Etsea et sit dolor
-                amet ipsum
-              </li>
-              <li class="py-2">
-                <i class="fa fa-check text-success mr-3"></i>Diam dolor diam
-                elitripsum vero.
-              </li>
-            </ul> -->
-          <!--  <a href="" class="btn btn-primary mt-4 py-2 px-4">Book Now</a>-->
-        </div>
-
-        <div class="col-lg-4">
-          <!-- <div class="card-header bg-secondary text-center p-4"> -->
-          <!-- <h1 class="text-white m-0">Book A Seat</h1>
-              </div>
-
-              <div class="card-body rounded-bottom bg-primary p-5">
-                <form>
-                  <div class="form-group">
-                    <input
-                      type="text"
-                      class="form-control border-0 p-4"
-                      placeholder="Your Name"
-                      required="required"
-                    />
-                  </div>
-                  <div class="form-group">
-                    <input
-                      type="email"
-                      class="form-control border-0 p-4"
-                      placeholder="Your Email"
-                      required="required"
-                    />
-                  </div>
-                  <div class="form-group">
-                    <select
-                      class="custom-select border-0 px-4"
-                      style="height: 47px"
-                    >
-                      <option selected>Select A Class</option>
-                      <option value="1">Class 1</option>
-                      <option value="2">Class 1</option>
-                      <option value="3">Class 1</option>
-                    </select>
-                  </div>
-                  <div>
-                    <button
-                      class="btn btn-secondary btn-block border-0 py-3"
-                      type="submit"
-                    > 
-                      Book Now
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>-->
         </div>
       </div>
     </div>
   </div>
   <!-- Registration End -->
 
-  <!-- Footer Start -->
-  <div
-    class="container-fluid bg-secondary text-white mt-5 py-5 px-sm-3 px-md-5">
-    <div class="row pt-5">
-      <div class="col-lg-3 col-md-6 mb-5">
-        <a
-          href=""
-          class="navbar-brand font-weight-bold text-primary m-0 mb-4 p-0"
-          style="font-size: 40px; line-height: 40px">
-          <!--<i class="flaticon-043-teddy-bear"></i>-->
-          <span class="text-white">CEA Cámbulos</span>
-        </a>
-        <p>
-          Colegio Cristiano, Educación Preescolar, Primaria, Secundaria, Bachillerato,
-          Valores, Cristiana, Cali, Colombia.
-          Reconocimiento Oficial de Estudios No. 672 del 16/06/1999
-          Nueva Resolución 2763 del 19/08/2004
-        </p>
-        <div class="d-flex justify-content-start mt-4">
-          <a
-            class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
-            style="width: 38px; height: 38px"
-            href="#"><i class="fab fa-twitter"></i></a>
-          <a
-            class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
-            style="width: 38px; height: 38px"
-            href="#"><i class="fab fa-facebook-f"></i></a>
-          <a
-            class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
-            style="width: 38px; height: 38px"
-            href="#"><i class="fab fa-linkedin-in"></i></a>
-          <a
-            class="btn btn-outline-primary rounded-circle text-center mr-2 px-0"
-            style="width: 38px; height: 38px"
-            href="#"><i class="fab fa-instagram"></i></a>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 mb-5">
-        <h3 class="text-primary mb-4">Contacto</h3>
-        <div class="d-flex">
-          <h4 class="fa fa-map-marker-alt text-primary"></h4>
-          <div class="pl-3">
-            <h5 class="text-white">Dirección</h5>
-            <p>Carrera 44 No. 9-25 Barrio: Cámbulos – Cali – Colombia</p>
-          </div>
-        </div>
-        <div class="d-flex">
-          <h4 class="fa fa-envelope text-primary"></h4>
-          <div class="pl-3">
-            <h5 class="text-white">Email</h5>
-            <p>info@cea.edu.co</p>
-          </div>
-        </div>
-        <div class="d-flex">
-          <h4 class="fa fa-phone-alt text-primary"></h4>
-          <div class="pl-3">
-            <h5 class="text-white">Teléfono</h5>
-            <p>314 8714906</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 mb-5">
-        <h3 class="text-primary mb-4">Acceso Rápido</h3>
-        <div class="d-flex flex-column justify-content-start">
-          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Inicio</a>
-          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Conócenos</a>
-          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Admisiones</a>
-          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Proyectos</a>
-          <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Finanzas</a>
-          <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Contacto</a>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 mb-5">
-        <h3 class="text-primary mb-4">Boletín</h3>
-        <form action="">
-          <div class="form-group">
-            <input
-              type="text"
-              class="form-control border-0 py-4"
-              placeholder="Nombre completo"
-              required="required" />
-          </div>
-          <div class="form-group">
-            <input
-              type="email"
-              class="form-control border-0 py-4"
-              placeholder="Email"
-              required="required" />
-          </div>
-          <div>
-            <button
-              class="btn btn-primary btn-block border-0 py-3"
-              type="submit">
-              Enviar
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-    <div
-      class="container-fluid pt-5"
-      style="border-top: 1px solid rgba(23, 162, 184, 0.2) ;">
-      <!--<p class="m-0 text-center text-white">
-          &copy;
-          <a class="text-primary font-weight-bold" href="#">Your Site Name</a>.
-          All Rights Reserved.-->
-
-      <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-      <!--Designed by
-          <a class="text-primary font-weight-bold" href="https://htmlcodex.com"
-            >HTML Codex</a
-          >-->
-      <!--<br />Distributed By:
-          <a href="https://themewagon.com" target="_blank">ThemeWagon</a>-->
-      </p>
-    </div>
-  </div>
-  <!-- Footer End -->
-
-  <!-- Back to Top -->
-  <a href="#" class="btn btn-primary p-3 back-to-top"><i class="fa fa-angle-double-up"></i></a>
-
-  <!-- JavaScript Libraries -->
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-  <script src="lib/easing/easing.min.js"></script>
-  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-  <script src="lib/isotope/isotope.pkgd.min.js"></script>
-  <script src="lib/lightbox/js/lightbox.min.js"></script>
-
-  <!-- Contact Javascript File -->
-  <script src="mail/jqBootstrapValidation.min.js"></script>
-  <script src="mail/contact.js"></script>
-
-  <!-- Template Javascript -->
-  <script src="js/main.js"></script>
+  <!-- Incluir Footer -->
+  <?php include 'footer.html'; ?>
 </body>
 
 </html>
