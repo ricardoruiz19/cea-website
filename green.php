d3332c2efe015cb052c7b1a761aa35b282b87110
moccad3v <jdavidpascuas@gmail.com>
2025-02-05 16:28:52 -0500

    Created green project view, added on navbar navigation

---
+++ green.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Educación adventista</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="Free HTML Templates" name="keywords" />
+    <meta content="Free HTML Templates" name="description" />
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon" />
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Handlee&family=Nunito&display=swap"
+        rel="stylesheet" />
+
+    <!-- Font Awesome -->
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
+        rel="stylesheet" />
+
+    <!-- Flaticon Font -->
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet" />
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <?php include 'navbar.html'; ?>
+
+    <!-- Header Start -->
+    <div class="container-fluid bg-secondary mb-2">
+        <div
+            class="d-flex flex-column align-items-center justify-content-center"
+            style="min-height: 100px">
+            <h3 class="display-3 font-weight-bold text-white mt-3">CEA Verde</h3>
+        </div>
+    </div>
+    <!-- Header End -->
+
+    <!-- Detail Start -->
+    <div class="container py-2">
+        <div class="row pt-5">
+            <div class="col-lg-12 text-center">
+                <div class="d-flex flex-column text-left mb-3">
+                    <p class="section-title pr-5">
+                        <span class="pr-2">Detalle del proyecto</span>
+                    </p>
+                    <h4 class="text-center">En construcción</h4>
+                </div>
+                <div class="mb-5">
+                    <img
+                        class="img-fluid rounded mb-4"
+                        src="img/code.jpg"
+                        alt="Image" />
+                    <p>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Detail End -->
+
+    <!-- Footer Start -->
+    <?php include 'footer.html'; ?>
+    <!-- Footer End -->
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary p-3 back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
